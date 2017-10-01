275ef75b64b670cd497793eb001b7c5520475972
Sebastian King <sebastianking@Sebastians-Mac.local>
2017-10-01 04:03:09 -0500

    ios version

--- Charald/ViewController.swift
+++ Charald/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,12 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func login_submit(_ sender: UIButton) {
+        if email.text == "" || password.text == "" {
+            // either textfield 1 or 2's text is empty
+        }
+    }
 }
+
 
