80d0a821f69a1fb0f97495e62b078a80ece41a6a
hamzahn <32418998+hamzahn@users.noreply.github.com>
2017-10-01 02:14:17 -0500

    (Not Working)Login/Register webview

--- app/src/main/java/com/nawfer/charald/LoginActivity.java
+++ app/src/main/java/com/nawfer/charald/LoginActivity.java
@@ -4,14 +4,17 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 
 import com.nawfer.charald.R;
 
 public class LoginActivity extends AppCompatActivity {
 
-    Button registerButton;
     Button loginButton;
+    Button registerButton;
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,17 +25,23 @@ public class LoginActivity extends AppCompatActivity {
         loginButton = (Button)findViewById(R.id.loginButton);
 
         registerButton.setOnClickListener((view) -> {
+            /*
             String url= "https://www.charald.com/#register";
             Intent intent = new Intent(Intent.ACTION_VIEW);
             intent.setData(Uri.parse("https://www.charald.com/#register"));
             startActivity(intent);
+            */
+            webView.loadUrl("https://www.charald.com/#register");
         });
 
         loginButton.setOnClickListener((view) -> {
+            webView.loadUrl("https://www.charald.com/#register");
+            /*
             String url= "https://www.charald.com/#register";
             Intent intent = new Intent(Intent.ACTION_VIEW);
             intent.setData(Uri.parse("https://www.charald.com/#login"));
             startActivity(intent);
+            */
         });
 
     }
