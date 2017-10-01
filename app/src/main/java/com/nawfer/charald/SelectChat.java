a465d963d1ba1e6ec410cf5697cb5e5a1c7b920b
hamzahn <32418998+hamzahn@users.noreply.github.com>
2017-09-30 21:01:16 -0500

    Will check for devices already paired and add them to map "PairedDevices"

--- app/src/main/java/com/nawfer/charald/SelectChat.java
+++ app/src/main/java/com/nawfer/charald/SelectChat.java
@@ -1,15 +1,19 @@
 package com.nawfer.charald;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.Set;
+
 public class SelectChat extends AppCompatActivity {
 
     private final static int REQUEST_ENABLE_BT = 1;
+    BluetoothAdapter btAdapter;
 
     /*
     This view will be where bluetooth devices are scanned and chosen to
@@ -22,7 +26,7 @@ public class SelectChat extends AppCompatActivity {
 
         //Check if device has bluetooth feature
         //If not, shut down app
-        BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
         if (btAdapter == null)
         {
             //If device not bluetooth enabled -> show dialog box -> shut down app
@@ -55,6 +59,23 @@ public class SelectChat extends AppCompatActivity {
 
     private void scanButtonClick(View view)
     {
+        /*Will scan for nearby bluetooth devices
+        NOTE: Paired /= Connected
+              (known)   (send/receive info)
+          1) Query paired device
+             a) Query will return name and MAC address of paired devices
+             b) Can be used for previous chats
+             c) Previous chats can be saved with an instance state OR .txt
+                i) instance states found in "Dictionary2.0 - MainActivity"
+          2) Discover Devices
+        */
+
+        //1. Query Paired Devices
+        Set<BluetoothDevice> pairedDevices = btAdapter.getBondedDevices();
+        if (pairedDevices.size() > 0)
+        {
+
+        }
 
     }
 }
