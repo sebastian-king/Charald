c655576e40e9703fa51a347134007aa4d1841593
Sebastian King <sebastianking@Sebastians-Mac.local>
2017-10-01 06:39:22 -0500

    login is in early experimental stages, sleep time

--- Charald/ViewController.swift
+++ Charald/ViewController.swift
@@ -7,11 +7,22 @@
 //
 
 import UIKit
+import Foundation
+
+/*
+ let url = URL(string: "https://www.charald.com/test-get.txt")
+ let task = URLSession.shared.dataTask(with: url!) {(data, response, error) in
+ let str = String(data: data!, encoding: String.Encoding.utf8) as String!;
+ self.status_label.text = str;
+ }
+ task.resume() */
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var email: UITextField!
     @IBOutlet weak var password: UITextField!
+    
+    @IBOutlet weak var status_label: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,10 +35,24 @@ class ViewController: UIViewController {
     }
     
     @IBAction func login_submit(_ sender: UIButton) {
-        if email.text == "" || password.text == "" {
-            // either textfield 1 or 2's text is empty
+        if (email.text == "") {
+            status_label.text = "You must enter your e-mail address.";
+        } else if (password.text == "") {
+            status_label.text = "You must enter your password."
+        } else {
+            self.status_label.text = "Loading..."
+            var request = URLRequest(url:URL(string:"https://www.charald.com/post-test.php")!)
+            request.httpMethod = "POST"
+            
+            let params = ["email":email.text, "password":password.text]
+            request.httpBody = try? JSONSerialization.data(withJSONObject: params, options: [])
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            
+            let task = URLSession.shared.dataTask(with: request) { (data:Data?, response:URLResponse?, error:Error?) in
+                let str = String(data: data!, encoding: String.Encoding.utf8) as String!;
+                self.status_label.text = str;
+            }
+            task.resume()
         }
     }
 }
-
-
